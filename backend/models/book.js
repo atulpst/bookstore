a7b667a38773c00d4c7514d09bfd7cf48f3a0907
Atul Pandey <atulpandey@Atuls-MacBook-Air.local>
2025-12-06 22:41:15 +0530

    Update Book Model to include coverImage

--- backend/models/book.js
+++ backend/models/book.js
@@ -1,11 +1,10 @@
-// backend/models/book.js
-
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
     title: { type: String, required: true },
     author: { type: String, required: true },
-    price: { type: Number, required: true }, // Price in INR
+    price: { type: Number, required: true },
+    coverImage: { type: String } // <--- ADD THIS LINE
 });
 
 module.exports = mongoose.model('Book', bookSchema);
